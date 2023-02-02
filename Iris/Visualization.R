e206b4d0d2b0d2bc626ba2f24df27fdaac6858ff
AP Miravite <87432826+apmiravite@users.noreply.github.com>
2023-02-02 22:21:47 +0800

    Update Visualization.R

--- Iris/Visualization.R
+++ Iris/Visualization.R
@@ -1,3 +1,35 @@
 library(tidyverse) 
 data <- data('iris') #loads data
 summary(iris)
+
+# Let's see an overview of the dimensions of the flowers, for each species of iris
+boxplot(Sepal.Length~Species,
+        data=iris,
+        main='Sepal Length by Species',
+        xlab='Species',
+        ylab='Sepal Length',
+        col='steelblue',
+        border='black')
+boxplot(Sepal.Width~Species,
+        data=iris,
+        main='Sepal Width by Species',
+        xlab='Species',
+        ylab='Sepal width',
+        col='steelblue',
+        border='black')
+
+boxplot(Petal.Width~Species,
+        data=iris,
+        main='Petal Width by Species',
+        xlab='Species',
+        ylab='Petal width',
+        col='steelblue',
+        border='black')
+
+boxplot(Petal.Length~Species,
+        data=iris,
+        main='Petal length by Species',
+        xlab='Species',
+        ylab='Petal length',
+        col='steelblue',
+        border='black')
